46504d77356cea8f93bbc57423254bd72cddb15e
saigyou_ayakashi <minuit1993@live.fr>
2023-02-23 11:49:51 +0100

    Add sentry

--- apps/front/src/main.tsx
+++ apps/front/src/main.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import { StyledEngineProvider, ThemeProvider, createTheme } from "@mui/material/styles";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -13,6 +15,12 @@ const darkTheme = createTheme({
   palette: {
     mode: "dark",
   },
+});
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
 });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
