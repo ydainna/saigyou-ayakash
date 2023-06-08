3743e6b968c6f7bc2fb697e9ad23dab0f9636fea
saigyou_ayakashi <minuit1993@live.fr>
2023-06-08 11:14:06 +0200

    Fix sentry bug

--- apps/front/src/main.tsx
+++ apps/front/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
 import { StyledEngineProvider, ThemeProvider, createTheme } from "@mui/material/styles";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -20,8 +19,21 @@ const darkTheme = createTheme({
 
 Sentry.init({
   dsn: constants.SENTRY_DSN as string,
-  integrations: [new BrowserTracing()],
+  integrations: [
+    new Sentry.BrowserTracing({
+      tracePropagationTargets: ["all"],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
 });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
