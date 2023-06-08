a0139b6022a1f6db54500f16206a68c043092391
saigyou_ayakashi <minuit1993@live.fr>
2023-06-08 11:51:44 +0200

    add mapping for sentry

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -1,10 +1,25 @@
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { env } from "process";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
+  plugins: [
+    react(),
+    // Put the Sentry vite plugin after all other plugins
+    sentryVitePlugin({
+      org: "me-0v",
+      project: "saigyou-front",
+      // Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
+      // and need `project:releases` and `org:read` scopes
+      authToken: env.SENTRY_AUTH_TOKEN,
+    }),
+  ],
   resolve: {
     alias: {
       "@assets": path.resolve(__dirname, "src/assets"),
