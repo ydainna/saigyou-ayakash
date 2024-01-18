2231ca3d906631c597478365db3a87eeafe3275f
ydainna <minuit1993@live.fr>
2024-01-18 15:21:43 +0100

    update page main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="bg-body min-h-screen">
-      <button className="bg-button-primary text-white font-bold py-2 px-4 rounded">Butdsfdsfton</button>
+      <button className="bg-button-primary text-white font-bold py-2 px-4 rounded">Button</button>
       <button className="bg-button-secondary text-white font-bold py-2 px-4 rounded">Button</button>
     </main>
   );
