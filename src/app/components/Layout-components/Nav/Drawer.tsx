806444a18c9f7720d04892598d71a2f412002640
ydainna <minuit1993@live.fr>
2024-02-02 14:58:52 +0100

    Remove "Menu" title from Drawer component

--- src/app/components/Layout-components/Nav/Drawer.tsx
+++ src/app/components/Layout-components/Nav/Drawer.tsx
@@ -10,7 +10,6 @@ export default function Drawer(props: DrawerProps) {
   return (
     <div className="z-100 absolute bg-black bg-opacity-50 h-full w-full" onClick={() => onClose()}>
       <div className="z-101 left-0 bg-paper text-white h-full w-60 pt-2 flex flex-col items-center" onClick={(event) => event.stopPropagation()}>
-        <div className="font-bold text-2xl">Menu</div>
         <div className="flex flex-col space-y-2 mt-4 text-primary">
           {items.map((item) => (
             <button key={item.title} className="flex flex-row space-x-5 relative text-md hover:bg-hover-nav-button px-1.5 py-2 rounded">
